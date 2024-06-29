45c06782383ba0f64567847da9e4708944b44a4e
Roi-flores13 <roi.flores@iteso.mx>
2024-06-29 17:26:44 -0600

    Optimized 125 valid palidrome

--- 125_valid_palindrome.py
+++ 125_valid_palindrome.py
@@ -2,14 +2,9 @@ s = "A man, a plan, a canal: Panama"
 
 def isPalindrome(s):
     s_altered = "".join(letter for letter in s.lower() if letter.isalnum())
-    left = 0
-    right = len(s_altered) - 1
     
-    for i in range(len(s_altered)):
-        if s_altered[left] !=  s_altered[right]:
-            return False
-        left += 1
-        right -= 1
+    if s_altered != s_altered[::-1]:
+        return False
     
     return True
 
